36ec50d85de27cc92a9bfae98fc962e8e8f0f5d0
Gmarson <gabrielmarson@live.com>
2015-12-02 16:54:19 -0200

    Trapézios modificados. Foi adicionado a verificação de erro

--- Trabalho3/Integração_Trapezio.R
+++ Trabalho3/Integração_Trapezio.R
@@ -1,23 +1,41 @@
-trapezio <-function(a,b)
+f <- function(x)
 {
-	a <- 0
-	b <- 0.8
+  return (0.2 + 25*x - 200*(x^2) + 675* (x^3) - 900 * (x^4) + 400* (x^5)  )
+}
 
 
-	f(x) %as% {0.2 + 25*x - 200*(x^2) + 675* (x^3) - 900 * (x^4) + 400* (x^5)}
-	g(x) %as% {integrate(f,a,b)}
+f2 <- function(derivada_segunda,x)
+{
+  return (eval(derivada_segunda))
+}
 
-	Raprox <- (b-a) * (( f(a) + f(b) ) /(2))
-	Rreal <- g(b)$value
+a <- 0
+b <- 0.8
+e <- expression(0.2 + 25*x - 200*(x^2) + 675* (x^3) - 900 * (x^4) + 400* (x^5))
 
+#ERRO RELATIVO --------------------------------------
+g <- integrate(f,a,b)$value
+Raprox <- (b-a) * (( f(a) + f(b) ) /(2))
+Rreal <- g
+erroR <- abs((Rreal - Raprox)/Rreal) * 100
+# ----------------------------------------------------
 
-	erroR <- abs((Rreal - Raprox)/Rreal) * 100
+#ERRO DA INTEGRAÇÃO ----------------------------------
+derivada_segunda <- D( D(e,"x"), "x" )
+intervalo <- seq(a,b,length.out=1000)
 
+x <- abs(f2(derivada_segunda,intervalo[1]))
+for(i in 2:(length(intervalo)))
+{
 
-	cat("Valor aproximado",Raprox,"\nValor Real:",Rreal)
-	cat("\nO erro eh de ",erroR,"%")
-
-
-
+  if(x < abs(f2(derivada_segunda,intervalo[i])))
+		x <- abs(f2(derivada_segunda,intervalo[i]))
 
 }
+
+Et <- - ( (  ( (b-a)^3) ) / 12) * x
+##---------------------------------------------------
+
+cat("Valor aproximado",Raprox,"\nValor Real:",Rreal)
+cat("\nO erro eh de ",erroR,"%")
+cat("\nErro da integração: ",Et)
